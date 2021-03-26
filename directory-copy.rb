56568e670608478c629774db436737e0f9434d77
fizzy-fifs <fkinyemi@gmail.com>
2021-03-26 18:42:20 +0000

    More refactoring

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,26 +1,35 @@
-def interactive_menu
-  students = []
-  loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input and save it into a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit #this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
+end
+
+def interactive_menu
+  loop do    
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def spellcheck(cohort)
@@ -43,32 +52,13 @@ def spellcheck(cohort)
 end
 
 def input_students
-  puts "Please enter the student's name"
-  name = gets.gsub("\n","")
-  puts "Please enter the student's age"
-  age = gets.gsub("\n","")
-  puts "Please enter the student's height"
-  height = gets.gsub("\n","")
-  puts "Please enter the student's country of birth"
-  c_o_b = gets.gsub("\n","")
-  puts "Please enter the student's cohort"
-  cohort = gets.gsub("\n","").to_sym
-  cohort = spellcheck(cohort)
-  if cohort.empty?
-    cohort = :unknown
-  end
   # create an empty array
-  students = []
-
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: cohort, age: age, height: height, c_o_b: c_o_b}
-    puts "Now we have #{students.count} students"
-  
+  @students = []
+  while true do
     #get another student info from the user
     puts "Please enter the student's name. Hit return again to finish"
     name = gets.gsub("\n","")
+    break if name.empty?
     puts "Please enter the student's age"
     age = gets.gsub("\n","")
     puts "Please enter the student's height"
@@ -81,14 +71,15 @@ def input_students
     if cohort.empty?
       cohort = :unknown
     end
+    #add the student hash to the array
+    @students << {name: name, cohort: cohort, age: age, height: height, c_o_b: c_o_b}
+    puts "Now we have #{@students.count} students"
   end
-  #return the array of students
-  students
 end
 
 def cohort(students)
   cohorts = []
-  students.each do |x| 
+  @students.each do |x| 
     if cohorts.empty?
       cohorts << x[:cohort]
     elsif cohorts.include? x[:cohort]
@@ -101,9 +92,9 @@ def cohort(students)
 end
 
 def print_by_cohort(students)
-  cohort_list = cohort(students)
+  cohort_list = cohort(@students)
   cohort_list.each do |month|
-    students.each do |x| 
+    @students.each do |x| 
       if x[:cohort] == month
         puts "#{x[:name].center(5)}; Age: #{x[:age].center(5)}; Height: #{x[:height].center(5)}; Born in #{x[:c_o_b].center(5)} (#{x[:cohort]} cohort)".center(10)
       else
@@ -118,14 +109,14 @@ def print_header
   puts "-------------------"
 end
 
-def print(students)
+def print_students_list
   while true do
-    students.each do |student|
+    @students.each do |student|
       if student[:name] == nil
         puts "You have not entered a student name"
       end
     end
-    students.select.with_index(1) do |student, index| 
+    @students.select.with_index(1) do |student, index| 
       if student[:name].length < 12
         puts "#{index} #{student[:name].center(5)}; Age: #{student[:age].center(5)}; Height: #{student[:height].center(5)}; Born in #{student[:c_o_b].center(5)} (#{student[:cohort]} cohort)".center(10)
       end
@@ -134,16 +125,15 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student"
-  elsif names.count == 0
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
+  elsif @students.count == 0
     puts "There are no students info in the directory"
   else
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
-
 
 #nothing happens until we call the methods
 interactive_menu 
